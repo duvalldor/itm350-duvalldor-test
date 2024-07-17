07aa78f2dd203b05de1d915e77db3927d7f33f89
Doreen Duvall <65792494+duvalldor@users.noreply.github.com>
2024-07-17 15:30:18 -0700

    Added code to app.js and backend/api.js

--- bb/bulletin-board-app/backend/api.js
+++ bb/bulletin-board-app/backend/api.js
@@ -1,9 +1,100 @@
+
+const statusCodes = require('http').STATUS_CODES;
+const httpConstants = require('http2').constants;
+
+// Include the AWS SDK module
+const AWS = require('aws-sdk');
+// Set the region
+AWS.config.update({ region: 'us-east-1' });
+
+
+
+// Instantiate a DynamoDB document client with the SDK
+let dynamodb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+
+
+let params = {
+  AttributeDefinitions: [
+    {
+      AttributeName: "TITLE",
+      AttributeType: "S",
+    },
+    {
+      AttributeName: "DETAILS",
+      AttributeType: "S",
+    },
+    {
+      AttributeName: "DATE",
+      AttributeType: "S",
+    },
+  ],
+  KeySchema: [
+    {
+      AttributeName: "ID",
+      KeyType: "HASH",
+    },
+    {
+      AttributeName: "TITLE",
+      KeyType: "RANGE",
+    },
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 2,
+    WriteCapacityUnits: 2,
+  },
+  TableName: "EVENT_LIST",
+  StreamSpecification: {
+    StreamEnabled: false,
+  },
+};
+
+// Call DynamoDB to create the table
+dynamodb.createTable(params, function (err, data) {
+  if (err) {
+    console.log("Error Creating " + params.TableName + " table", err);
+  } else {
+    console.log(params.TableName + " Table Created", data);
+  }
+});
+
+
+
+
+
+
+
+
+
+
 var events = require('./events.js');
 
 exports.events = function (req, res) {
   res.json(events);
 };
 
+
+
 exports.event = function (req, res) {
   res.json(events[req.param.eventId]);
+
+  if (req.params.title.trim()) {
+    let putparams = {
+      TableName: "EVENT_LIST",
+      Item: {
+        TITLE:   { S: req.params.title },
+        DETAILS: { S: req.params.details },
+        DATE:    { S: req.params.date },
+        ID:      { N: req.params.id },
+      },
+    };
+  
+    // Call DynamoDB to add the item to the table
+    dynamodb.putItem(putparams, function (err, data) {
+      if (err) {
+        console.log("Error adding item to table", err);
+      } else {
+        console.log("Success adding item to table", data);
+      }
+    });
+  }
 };
